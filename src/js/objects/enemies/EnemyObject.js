7a11420eb9d0e7eeb23f8a05d4ef06e574d0c624
Trey <102479767+treyboo3@users.noreply.github.com>
2023-11-14 14:06:09 -0500

    Enemy count is now reduced when they reach the end.

--- src/js/objects/enemies/EnemyObject.js
+++ src/js/objects/enemies/EnemyObject.js
@@ -177,6 +177,7 @@ reachedEnd() {
     // Check if the enemy has reached the end of the path
     if (Phaser.Math.Distance.Between(this.x, this.y, targetX, targetY) < 1) {
         this.scene.displayManager.playerHealthManager.reducePlayerHealth(this.damage);
+        this.scene.displayManager.enemyCountManager.reduceEnemy(1);
         this.destroy();
         return true;
     }
