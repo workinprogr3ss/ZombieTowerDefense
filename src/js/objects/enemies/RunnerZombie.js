da8583144f67c9b6c14b2e2d9c967c8c2d22cc02
Trey <102479767+treyboo3@users.noreply.github.com>
2023-11-14 14:43:15 -0500

    Added comment under each zombies for constructor variables.

--- src/js/objects/enemies/RunnerZombie.js
+++ src/js/objects/enemies/RunnerZombie.js
@@ -3,6 +3,7 @@ import Enemy from './EnemyObject.js';
 // Export Runner class
 export default class RunnerZombie extends Enemy {
     constructor(scene, x, y, initalDirection) {
+        //scene, x, y, texture, initialDirection, health, speed, damage, value
         super(scene, x, y, 'runnerZombieRight', initalDirection, 50, 50, 5, 25);
     }
 
--- src/js/objects/enemies/SpitterZombie.js
+++ src/js/objects/enemies/SpitterZombie.js
@@ -3,6 +3,7 @@ import Enemy from './EnemyObject.js';
 // Export Spitter class
 export default class SpitterZombie extends Enemy {
     constructor(scene, x, y, initalDirection) {
+        // scene, x, y, texture, initialDirection, health, speed, damage, value
         super(scene, x, y, 'spitterZombieRight', initalDirection, 100, 40, 15, 150);
     }
 
